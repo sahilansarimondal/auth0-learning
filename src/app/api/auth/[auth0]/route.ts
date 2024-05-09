f6eb0df3a0b5fb48bcf2485ccaa6af0a9e8a9877
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2024-05-09 19:57:05 +0530

    learning auth0 form new doc's

--- src/app/api/auth/[auth0]/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/auth/[auth0]/route.js
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
-
-export const GET = handleAuth({
-  login: handleLogin({
-    returnTo: "/dashboard",
-  }),
-});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useUser } from "@auth0/nextjs-auth0/client";
 import React from "react";
 import Link from "next/link";
 
